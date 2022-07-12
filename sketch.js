a503de84482539a5ade71f4173d03473e2c1b5e8
bosterho <48608512+bosterho@users.noreply.github.com>
2022-07-12 02:13:43 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let flowers = []
 function setup() {
   createCanvas(400, 400);
 
-  flowers.push(new Flower(100, 150, random(0, 200)))
+  flowers.push(new Flower(100, 200, random(0, 200)))
   flowers.push(new Flower(250, 90, random(0, 200)))
   flowers.push(new Flower(200, 250, random(0, 200)))
 }
